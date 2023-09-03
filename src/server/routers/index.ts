4582aa8eb2d2181331052c1ae82cc592e103b409
Raúl Pérez <jrpcassone@gmail.com>
2023-09-03 22:55:58 +0200

    Set-up cors middleware at routers index

--- src/server/routers/index.ts
+++ src/server/routers/index.ts
@@ -1,7 +1,17 @@
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
 
 export const app = express();
 app.disable("x-powered-by");
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:4000",
+      "https://raul-perez-final-project-202307-bcn.netlify.app/",
+    ],
+  }),
+);
 
 export default app;
