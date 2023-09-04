b564ca4230be52b581a799ce614279dc0c92c516
Raúl Pérez <jrpcassone@gmail.com>
2023-09-04 12:46:52 +0200

    Test '/' endpoint, integration test

---
+++ src/server/routers/__tests__/slashEndpoint.test.ts
@@ -0,0 +1,27 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import request from "supertest";
+import app from "../../index.js";
+
+let server: MongoMemoryServer;
+
+beforeAll(async () => {
+  server = await MongoMemoryServer.create();
+});
+
+afterAll(async () => {
+  await server.stop();
+});
+
+describe("Given a GET '/' endpoint", () => {
+  describe("When it receives a request", () => {
+    test("Then it should respond with a status 200 and message 'Pong 🏓", async () => {
+      const endpoint = "/";
+
+      await request(app).get(endpoint).expect(200);
+
+      const response = await request(app).get(endpoint);
+
+      expect(response.body).toHaveProperty("message", "Pong 🏓");
+    });
+  });
+});
