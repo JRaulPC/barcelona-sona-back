16442cad0bac617fc5e2cd689feed88953a4925e
Raúl Pérez <jrpcassone@gmail.com>
2023-09-04 10:38:18 +0200

    Create custom error class

--- src/server/routers/startServer.ts
+++ src/server/routers/startServer.ts
@@ -1,12 +1,12 @@
 import debugCreator from "debug";
 import "dotenv/config";
-import app from "./index.js";
+import app from "../index.js";
 
 const debug = debugCreator("spots:server");
 
 const startServer = (port: number) => {
   app.listen(port, () => {
-    debug(`Listening to on http://localhost:${port}/things`);
+    debug(`Listening to on http://localhost:${port}/espacios`);
   });
 };
 
