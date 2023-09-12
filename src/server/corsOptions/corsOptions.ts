1631f9f122aa78974cf347f52aeb447401e257bb
Raúl Pérez <jrpcassone@gmail.com>
2023-09-12 10:08:51 +0200

    Fix ref property on user schema property from spotSchema

--- src/server/corsOptions/corsOptions.ts
+++ src/server/corsOptions/corsOptions.ts
@@ -1,22 +1,11 @@
-import { type CorsOptions } from "cors";
 import "dotenv/config";
+import { type CorsOptions } from "cors";
 
 const localOriginUrl = process.env.CORS_ORIGIN_LOCAL!;
 const prodOriginUrl = process.env.CORS_ORIGIN_PROD!;
 
 const corsOptions: CorsOptions = {
-  origin: [localOriginUrl, prodOriginUrl],
-  allowedHeaders: [
-    "Content-Type",
-    "Origin",
-    "X-Requested-With",
-    "Accept",
-    "x-client-key",
-    "x-client-token",
-    "x-client-secret",
-    "Authorization",
-  ],
-  credentials: true,
+  origin: [prodOriginUrl, localOriginUrl],
 };
 
 export default corsOptions;
